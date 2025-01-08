3b572a69b562f6fafe03b41433448a2bf16ad216
Rahul Anandh <rahulanandh80@gmail.com>
2025-01-09 02:19:27 +0530

    api

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var bodyParser = require("body-parser");
 // const  dotenv = require ("dotenv");
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
+var cors = require("cors");
 
 // const PORT = process.env.PORT;
 // const DB_URL = process.env.DB_URL;
@@ -17,6 +18,8 @@ const userRouters = require("./routes/user");
 app.listen(PORT, () => {
   console.log(`SERVER STARTED ON http://localhost:${PORT}`);
 });
+app.use(cors());
+
 mongoose
   .connect(DB_URL)
   .then(() => console.log(`SERVER CONNECTED TO ${DB_URL}`));
